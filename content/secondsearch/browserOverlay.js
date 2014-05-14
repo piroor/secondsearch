ec196afbcade8556a8b6ab5796a847d09d016843
Piro / YUKI Hiroshi <piro.outsider.reflex@gmail.com>
2014-05-14 20:16:13 +0900

    Extract "eval()" codes

--- content/secondsearch/browserOverlay.js
+++ content/secondsearch/browserOverlay.js
@@ -471,100 +471,7 @@ SecondSearchBrowser.prototype = inherit(SecondSearchBase.prototype, {
 			textbox.onKeyPress = this.onTextboxKeyPress.bind(this);
 		}
 
-		if (search.localName == 'searchbar') { // search bar
-			if ('handleSearchCommand' in search && !search.__secondsearch__doSearch) {
-				let target = 'search.handleSearchCommand';
-				let func = search.handleSearchCommand;
-
-				// compatibility for Searchbar Autosizer
-				// https://addons.mozilla.org/firefox/addon/searchbar-autosizer/
-				if ('autosizer' in this.window && autosizer.originalHandleSearchCommand) {
-					target = 'autosizer.originalHandleSearchCommand';
-					func = autosizer.originalHandleSearchCommand;
-				}
-
-				// compatibility for Tab Control
-				// https://addons.mozilla.org/firefox/addon/tab-control/
-				if ('gTabControl' in this.window && gTabControl.origHandleSearchCommand) {
-					target = 'gTabControl.origHandleSearchCommand';
-					func = gTabControl.origHandleSearchCommand;
-				}
-
-				// compatibility for SearchLoad Options
-				// https://addons.mozilla.org/firefox/addon/searchload-options/
-				if ('esteban_torres' in this.window &&
-					'searchLoad_Options' in esteban_torres &&
-					esteban_torres.searchLoad_Options.MOZhandleSearch) {
-					target = 'esteban_torres.searchLoad_Options.MOZhandleSearch';
-					func = esteban_torres.searchLoad_Options.MOZhandleSearch;
-				}
-
-				eval(target+' = '+func.toSource().replace(
-					')',
-					', aOverride)'
-				).replace(
-					/doSearch\(([^\)]+)\)/,
-					'doSearch($1, aEvent, aOverride)'
-				));
-				eval('search.doSearch = '+search.doSearch.toSource().replace(
-					'{',
-					'$& window.' + this.name + '.readyToSearch();'
-				).replace(
-					/(\}\)?)$/,
-					'window.' + this.name + '.searchDone(); $1'
-				));
-				search.__secondsearch__doSearch = search.doSearch;
-				search.doSearch = this.doSearchbarSearch;
-				search._popup.addEventListener('command', this, true);
-			}
-
-			// old Tab Mix Plus, only Firefox 2?
-			if ('handleSearchCommand' in search &&
-				'TMP_SearchLoadURL' in this.window && !this.window.__secondsearch__TMP_SearchLoadURL) {
-				this.window.__secondsearch__TMP_SearchLoadURL = this.window.TMP_SearchLoadURL;
-				eval('window.TMP_SearchLoadURL = '+window.TMP_SearchLoadURL.toSource().replace(
-					'var submission = searchbar.currentEngine',
-					'var overrideEngine = null;' +
-					'if (window.' + this.name + '.selectedEngine) {' +
-					'  overrideEngine = window.' + this.name + '.getSearchEngineFromName(window.' + this.name + '.selectedEngine.name);' +
-					'};' +
-					'var submission = (overrideEngine || searchbar.currentEngine)'
-				));
-			}
-		}
-		else { // location bar
-			if (textbox.onDrop &&
-				!textbox.__secondsearch__updated) {
-				eval('textbox.onDrop = '+textbox.onDrop.toSource().replace(
-					'{',
-					'{' +
-					'  var ss = window.' + this.name + ';' +
-					'  ss.droppedURI = null;' +
-					'  var showSecondSearch = false;' +
-					'  if ((typeof aXferData == "undefined" ? ' +
-					'         ss.getDroppedText(aEvent) : ' +
-					'         aXferData.flavour.contentType == "text/unicode") &&' +
-					'    ss.autoShowDragdropMode == ss.DRAGDROP_MODE_DROP) {' +
-					'    showSecondSearch = (ss.searchbar == this);' +
-					'  }'
-				).replace(
-					'return;',
-					'if (showSecondSearch)' +
-					'  ss.showSecondSearch(ss.SHOWN_BY_DROP);' +
-					'$&'
-				).replace(
-					/((handleURLBarCommand|this\.handleCommand)\(\);)/,
-					'if (showSecondSearch) {' +
-					'  ss.droppedURI = this.value;' +
-					'  ss.showSecondSearch(ss.SHOWN_BY_DROP);' +
-					'}' +
-					'else {' +
-					'  $1;' +
-					'}'
-				));
-				textbox.__secondsearch__updated = true;
-			}
-		}
+		this.window.SecondSearchWindowHelper.initBar(this);
 
 		this.window.setTimeout(function(aSelf) {
 			aSelf.testOpenPopup();
@@ -1637,17 +1544,7 @@ SecondSearchBrowser.prototype = inherit(SecondSearchBase.prototype, {
 		this.window.removeEventListener('DOMContentLoaded', this, false);
 		this.window.addEventListener('load', this, false);
 
-		// compatibility for Tab Control
-		// https://addons.mozilla.org/firefox/addon/tab-control/
-		if ('gTabControl' in this.window && this.window.gTabControl.handleSearchCommand) {
-			eval('gTabControl.handleSearchCommand = '+this.window.gTabControl.handleSearchCommand.toSource().replace(
-				')',
-				', aOverride)'
-			).replace(
-				'[aEvent]',
-				'[aEvent, aOverride]'
-			));
-		}
+		this.window.SecondSearchWindowHelper.preInit(this);
 	},
  
 	init : function SSBrowser_init() 
