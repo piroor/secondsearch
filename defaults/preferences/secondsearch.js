4fb9ea83498a7bfe0f52c92483d0c44be6d95c41
YUKI Hiroshi <shimoda@clear-code.com>
2015-02-07 22:18:58 +0900

    Revert "Use human readable id for preference keys"
    This reverts commit 649a941fc670489f72ebaff277307d98a29bf3de.

--- defaults/preferences/secondsearch.js
+++ defaults/preferences/secondsearch.js
@@ -1,30 +1,30 @@
-pref("extensions.secondsearch@piro.sakura.ne.jp.popup.auto_show", true);
+pref("extensions.{0AE5CAA4-8BAB-11DB-AF59-ED4B56D89593}.popup.auto_show", true);
 // 1 = up/down, 2 = Shift-up/down
-pref("extensions.secondsearch@piro.sakura.ne.jp.popup.manual_show.arrowKeys", 3);
-pref("extensions.secondsearch@piro.sakura.ne.jp.popup.auto_show.delay", 200);
+pref("extensions.{0AE5CAA4-8BAB-11DB-AF59-ED4B56D89593}.popup.manual_show.arrowKeys", 3);
+pref("extensions.{0AE5CAA4-8BAB-11DB-AF59-ED4B56D89593}.popup.auto_show.delay", 200);
 // 0 = nothing, 1 = on dragover, 2 = on drop (old)
-pref("extensions.secondsearch@piro.sakura.ne.jp.popup.auto_show.dragdrop.mode", 1);
-pref("extensions.secondsearch@piro.sakura.ne.jp.popup.auto_show.dragdrop.delay", 350);
-pref("extensions.secondsearch@piro.sakura.ne.jp.popup.position",     0); // 0 = above, 1 = below
-pref("extensions.secondsearch@piro.sakura.ne.jp.popup.type",         0); // 0 = recent used, 1 = all, 2 = all (reversed)
-pref("extensions.secondsearch@piro.sakura.ne.jp.popup.type.context",  1); // -1 = default
-pref("extensions.secondsearch@piro.sakura.ne.jp.popup.type.dragdrop", -1); // -1 = default
-pref("extensions.secondsearch@piro.sakura.ne.jp.recentengines.num",  5);
-pref("extensions.secondsearch@piro.sakura.ne.jp.switch.blank_input", true);
-pref("extensions.secondsearch@piro.sakura.ne.jp.keyword.show",       true);
-pref("extensions.secondsearch@piro.sakura.ne.jp.clear_after_search", true);
-pref("extensions.secondsearch@piro.sakura.ne.jp.clear_after_search.delay", 10000);
-pref("extensions.secondsearch@piro.sakura.ne.jp.reuse_blank_tab",    true);
-pref("extensions.secondsearch@piro.sakura.ne.jp.timeout",            5000);
-pref("extensions.secondsearch@piro.sakura.ne.jp.override.locationBar", true);
-pref("extensions.secondsearch@piro.sakura.ne.jp.ignoreAutoFillResult", true);
+pref("extensions.{0AE5CAA4-8BAB-11DB-AF59-ED4B56D89593}.popup.auto_show.dragdrop.mode", 1);
+pref("extensions.{0AE5CAA4-8BAB-11DB-AF59-ED4B56D89593}.popup.auto_show.dragdrop.delay", 350);
+pref("extensions.{0AE5CAA4-8BAB-11DB-AF59-ED4B56D89593}.popup.position",     0); // 0 = above, 1 = below
+pref("extensions.{0AE5CAA4-8BAB-11DB-AF59-ED4B56D89593}.popup.type",         0); // 0 = recent used, 1 = all, 2 = all (reversed)
+pref("extensions.{0AE5CAA4-8BAB-11DB-AF59-ED4B56D89593}.popup.type.context",  1); // -1 = default
+pref("extensions.{0AE5CAA4-8BAB-11DB-AF59-ED4B56D89593}.popup.type.dragdrop", -1); // -1 = default
+pref("extensions.{0AE5CAA4-8BAB-11DB-AF59-ED4B56D89593}.recentengines.num",  5);
+pref("extensions.{0AE5CAA4-8BAB-11DB-AF59-ED4B56D89593}.switch.blank_input", true);
+pref("extensions.{0AE5CAA4-8BAB-11DB-AF59-ED4B56D89593}.keyword.show",       true);
+pref("extensions.{0AE5CAA4-8BAB-11DB-AF59-ED4B56D89593}.clear_after_search", true);
+pref("extensions.{0AE5CAA4-8BAB-11DB-AF59-ED4B56D89593}.clear_after_search.delay", 10000);
+pref("extensions.{0AE5CAA4-8BAB-11DB-AF59-ED4B56D89593}.reuse_blank_tab",    true);
+pref("extensions.{0AE5CAA4-8BAB-11DB-AF59-ED4B56D89593}.timeout",            5000);
+pref("extensions.{0AE5CAA4-8BAB-11DB-AF59-ED4B56D89593}.override.locationBar", true);
+pref("extensions.{0AE5CAA4-8BAB-11DB-AF59-ED4B56D89593}.ignoreAutoFillResult", true);
 
-pref("extensions.secondsearch@piro.sakura.ne.jp.keyword.updating", false);
+pref("extensions.{0AE5CAA4-8BAB-11DB-AF59-ED4B56D89593}.keyword.updating", false);
 
-//pref("extensions.secondsearch@piro.sakura.ne.jp.loadInBackground", false);
+//pref("extensions.{0AE5CAA4-8BAB-11DB-AF59-ED4B56D89593}.loadInBackground", false);
 
 // for compatibility with Private Tab
 // https://addons.mozilla.org/firefox/addon/private-tab/
-pref("extensions.secondsearch@piro.sakura.ne.jp.openPrivateTab", false);
+pref("extensions.{0AE5CAA4-8BAB-11DB-AF59-ED4B56D89593}.openPrivateTab", false);
 
-pref("extensions.secondsearch@piro.sakura.ne.jp.handle_dragdrop_only_on_button", false);
+pref("extensions.{0AE5CAA4-8BAB-11DB-AF59-ED4B56D89593}.handle_dragdrop_only_on_button", false);
