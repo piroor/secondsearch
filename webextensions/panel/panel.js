02f52c95e955ae4bda49cd421cabf1ed2775db84
YUKI Hiroshi <shimoda@clear-code.com>
2017-11-20 14:36:38 +0900

    Add panel

---
+++ webextensions/panel/panel.js
@@ -0,0 +1,26 @@
+/*
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+'use strict';
+
+var gField;
+
+window.addEventListener('DOMContentLoaded', () => {
+  gField = document.querySelector('#search-field');
+  document.addEventListener('keypress', onKeyPress, { capture: true });
+}, { once: true });
+
+window.addEventListener('load', () => {
+  setTimeout(() => gField.focus(), 10);
+}, { once: true });
+
+window.addEventListener('pagehide', () => {
+  document.removeEventListener('keypress', onKeyPress, { capture: true });
+}, { once: true });
+
+
+function onKeyPress(aEvent) {
+  console.log('onKeyPress ', aEvent.keyCode);
+}
