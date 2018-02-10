a5dae0db003d4e45fc4c623b3b504dbd0738c3b1
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2018-02-10 23:33:56 +0900

    Add configuration UI to change keyboard shortcuts

--- webextensions/options/init.js
+++ webextensions/options/init.js
@@ -21,6 +21,28 @@ function onConfigChanged(aKey) {
 
 configs.$addObserver(onConfigChanged);
 window.addEventListener('DOMContentLoaded', () => {
+  browser.commands.getAll().then(aCommands => {
+    const fragment = document.createDocumentFragment();
+    for (let command of aCommands) {
+      const item = document.createElement('li');
+      const label = item.appendChild(document.createElement('label'));
+      label.textContent = `${command.description || command.name}:`;
+      const field = label.appendChild(document.createElement('input'));
+      field.setAttribute('type', 'text');
+      field.setAttribute('value', command.shortcut);
+      field.setAttribute('placeholder', command.shortcut);
+      field.addEventListener('input', () => {
+        browser.commands.update({
+          name:     command.name,
+          shortcut: field.value
+        });
+      });
+      fragment.appendChild(item);
+    }
+    document.getElementById('shortcuts').appendChild(fragment);
+    l10n.updateDocument();
+  });
+
   configs.$loaded.then(() => {
     options.buildUIForAllConfigs(document.querySelector('#debug-configs'));
     onConfigChanged('debug');
